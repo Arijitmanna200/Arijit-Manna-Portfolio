df1ccc96b322a9946decd583da58b09bc10c5519
Arijit Manna <10arijitmanna@gmail.com>
2025-12-19 22:32:13 +0530

    abcdefgh

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     setSuccess("");
 
     try {
-      const res = await fetch("https://arijit-manna-portfolio.vercel.app/api/send-mail", {
+      const res = await fetch("https://arijit-manna-portfolio-backend.vercel.app/api/send-mail", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
